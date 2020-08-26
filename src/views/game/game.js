88894c5a3c59d47d653bb8248396cd69ab6c62ed
Stephen Trewick <stephent818@gmail.com>
2020-08-25 20:24:56 -0700

    tried adding a branch platform

--- src/views/game/game.js
+++ src/views/game/game.js
@@ -10,6 +10,8 @@ import { get } from "lodash";
 const INIT_COMPLEXITY = 2;
 const { width, height } = Dimensions.get("window");
 
+let PLATFORM_COUNTER = 1;
+
 class Game extends PureComponent {
   static navigationOptions = {
     headerShown: false,
@@ -42,6 +44,13 @@ class Game extends PureComponent {
     return { obstacles, bodies };
   }
 
+  get branches() {
+    const branches = {};
+    for (let i = 0; i < PLATFORM_COUNTER; i++) {
+      const { branch, body } = this.branches;
+    }
+  }
+
   get initState() {
     return {
       complexity: INIT_COMPLEXITY,
@@ -63,14 +72,23 @@ class Game extends PureComponent {
       xtilt: 0,
       label: "jumper",
     });
-    Matter.World.add(world, [jumper]);
+    const branch = Matter.Bodies.rectangle(width / 2, height - 70, 35, 5, {
+      isStatic: true,
+      label: "branch",
+      isSensor: true,
+    });
+    const { branches, branchesInWorld } = this.branches;
+
+    Matter.World.add(world, [jumper, ...branchesInWorld]);
 
     return {
       physics: {
         engine,
         world,
       },
+      ...branches,
       jumper: { body: jumper, size: [50, 100], renderer: Jumper },
+      branch: { body: branch, size: [50, 15], renderer: Branch },
     };
   }
 
